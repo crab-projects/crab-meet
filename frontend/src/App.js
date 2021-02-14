16aa58131826539ad3513b5089d4efe30e88eb57
Mallik <MALLIAX1@abbvienet.com>
2021-02-13 18:00:36 -0600

    Set up React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './Home'
+import MakeMeeting from './MakeMeeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+        <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/make" component={MakeMeeting} />
+        </Switch>
+    </main>
+)
 }
 
 export default App;
