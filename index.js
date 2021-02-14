687116a696d9e005b4eac547d47be53c1461368d
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-02-14 00:14:31 -0600

    Restructure backend folder

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 const port = process.env.PORT || 5000 || '0.0.0.0';
