92b2d61feb3b4f39a8b2d60a4ae457f776dfa3a2
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-02-14 19:55:57 -0600

    🤝 Link database and express (#29)
    * Implement template postgres query for testing
    
    * Successfully connect database and add dotenv package
    
    Co-authored-by: Alec Chen <alecchendev@gmail.com>

--- backend/utils.js
+++ backend/utils.js
@@ -1,9 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
+//import { v4 as uuidv4 } from 'uuid';
+const uuidv4 = require('uuid').v4;
 
 const generatePassword = () => {
   var id = uuidv4();
   console.log(id);
   return id;
 }
+
+console.log(generatePassword());
 
 exports.generatePassword = generatePassword;
