2f4d8cde8fa2259c6eb1b5f2d83ced5b706b8401
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-02-16 20:04:22 -0600

    Complete initial dictionary parse

--- backend/utils.js
+++ backend/utils.js
@@ -1,10 +1,15 @@
 //import { v4 as uuidv4 } from 'uuid';
-const uuidv4 = require('uuid').v4;
+const uuidv4 = require("uuid").v4;
+const fs = require("fs");
 
 const generatePassword = () => {
   var id = uuidv4();
   console.log(id);
   return id;
-}
+};
 
 exports.generatePassword = generatePassword;
+
+const loadWords = () => {
+  const json_string = fs.readFileSync("./four_letter.json");
+};
