86a0f51271b56df8e290990ec6ad984dea9652d6
Alec Chen <30279834+alecchendev@users.noreply.github.com>
2021-02-20 16:11:33 -0600

    🤺 Implement functionality for inputting times (#79)
    * Implement edit versus view on calendar and render submit button to call meeting parent component submit function
    
    * Get rid of key errors in multiple nested components
    
    * Implement input for user name and placeholder function in api.js
    
    * Setup general structure for processing timeValues into date time strings
    
    * Restrict inputs to given window of start and end date and times
    
    * Implement processing times to send to backend

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -44,15 +44,84 @@ const ButtonBox = styled.div`
   margin: auto;
 `; 
 
-const Calendar = (startDate, endDate, startTime, endTime, timesInput, edit) => {
-
-  const [ timeValues, setTimeValues ] = React.useState(new Array(24 * 7).fill(false));
-
-  console.log(timeValues);
+const Calendar = (props) => {
+ 
+  const { meetingData, submitUserTimes, edit} = props;
 
   const [ mouseDown, setMouseDown ] = React.useState(false);
   const [ busy, setBusy ] = React.useState(false);
- 
+
+  const datetimeToDate = (date) => {
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const yyyy = date.getFullYear();
+    return [yyyy, mm, dd].join('-');
+  }
+
+  const datetimeToTime = (date) => {
+    return date.getHours() + ':' + date.getMinutes() + ':00';
+  }
+
+  /**
+   * Takes a string value time of format 'hh:mm:ss-tt'
+   * and converts to a date object of current time
+   */
+  const timeToDatetime = (time) => {
+    const datetime = new Date();
+    const [ hh, mm, ss ] = time.split(':');
+    datetime.setHours(hh, mm, ss.split('-')[0]);
+    return datetime;
+  }
+
+  const addDays = (date, days) => {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + days);
+    return newDate;
+  }
+
+  const addMinutes = (date, minutes) => {
+    return new Date(date.getTime() + minutes * 60 * 1000);
+  }
+
+  const defaultDate = datetimeToDate(addDays(new Date(), -3));
+  const defaultStartTime = '09:00:00-00';
+  const defaultEndTime = '17:00:00-00';
+  let { startDate, endDate, startTime, endTime, timeInputs } = meetingData;
+  startDate = startDate ? startDate : defaultDate;
+  endDate = endDate ? endDate : addDays(Date.parse(startDate), 3).toString();
+  startTime = startTime ? startTime : defaultStartTime;
+  endTime = endTime ? endTime : defaultEndTime;
+
+  const nDays = 7;
+  const timeIncr = 60; // minutes
+  const hours = 24;
+  const nTimes = hours * 60 / timeIncr;
+
+  const fillInitialTimes = (startDate, endDate, startTime, endTime, timeInputs) => {
+    // just account for a single week rn
+    // nullify days of week not in timeframe
+    // nullify times not in timeframe
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+    const startDatetime = timeToDatetime(startTime);
+    const endDatetime = timeToDatetime(endTime);
+    const timeVals = new Array(24 * 7).fill(false);
+    const startDateDay = startDate.getDay();
+    const endDateDay = endDate.getDay();
+    for (let day = 0; day < nDays; day++) {
+      for (let time = 0; time < nTimes; time++) {
+        const index = day * nTimes + time;
+        timeVals[index] = (day >= startDateDay && day <= endDateDay) ? false : null;
+        const thisDatetime = addMinutes(timeToDatetime('00:00:00-00'), timeIncr * time);
+        timeVals[index] = (thisDatetime >= startDatetime && thisDatetime < endDatetime) ? timeVals[index] : null;
+      }
+    }
+    return timeVals;
+  }
+
+  
+  const [ timeValues, setTimeValues ] = React.useState(() => fillInitialTimes(startDate, endDate, startTime, endTime, timeInputs));
+
   const handleMouseDown = (newBusy) => {
     setMouseDown(true);
     setBusy(newBusy);
@@ -64,10 +133,46 @@ const Calendar = (startDate, endDate, startTime, endTime, timesInput, edit) => {
 
   const handleDrag = (index) => {
     //console.log(index);
-    const newTimeValues = [...timeValues];
-    newTimeValues[index] = busy;
-    if (mouseDown)
-      setTimeValues(newTimeValues);
+    updateTimes(index);
+  }
+
+  const updateTimes = (index) => {
+    if (edit === true && timeValues[index] !== null) {
+      const newTimeValues = [...timeValues];
+      newTimeValues[index] = busy;
+      if (mouseDown)
+        setTimeValues(newTimeValues);
+    }
+  }
+  /**
+   * Convert timeValues ([true, false, true, ...]) to an array of
+   * strings in postgres date time format
+   */
+  const processTimes = () => {
+    const times = [];
+    for (let day = 0; day < nDays; day++) {
+      for (let time = 0; time < nTimes; time++) {
+        const index = day * nTimes + time;
+        if (timeValues[index]) {
+          const value = addMinutes(addDays(startDate, day), timeIncr * time);
+          let newTime = {
+            start: value,
+            end: addMinutes(value, timeIncr) // start + timeIncr
+          };
+          const prevTime = times[times.length - 1];
+          if (prevTime && prevTime.end.getTime() === newTime.start.getTime()) {
+            // merge this value into last time if it was right before
+            newTime = {
+              start: times[times.length - 1].start,
+              end: newTime.end
+            };
+            times.pop();
+          }
+          times.push(newTime);
+        }
+      }
+    }
+    submitUserTimes(times);
   }
 
   React.useEffect(() => {
@@ -75,10 +180,6 @@ const Calendar = (startDate, endDate, startTime, endTime, timesInput, edit) => {
   }, []);
 
 
-  const nDays = 7;
-  const timeIncr = 60;
-  const hours = 24;
-  const nTimes = hours * 60 / timeIncr;
   const rows = [];
   const times = [];
   for (let time = 0; time < nTimes; time++) {
@@ -87,26 +188,30 @@ const Calendar = (startDate, endDate, startTime, endTime, timesInput, edit) => {
       const index = day * nTimes + time;
       days.push(<td 
                   key={index}
-                  style={{background: (timeValues[index] ? '#ff9e78' : 'transparent')}}
+                  style={{background: (timeValues[index] === null ? 'var(--grey-2)' : timeValues[index] ? '#ff9e78' : 'transparent')}}
                   onMouseDown={() => handleMouseDown(!timeValues[index])}
                   onMouseUp={handleMouseUp}
                   onMouseOver={() => handleDrag(index)}>
                 </td>);
     }
-    times.push(<tr>{days}</tr>);
+    times.push(<tr key={time}>{days}</tr>);
   }
 
   return (
     <Wrapper>
       <div>
         <Grid>
-          {times}
+          <tbody>
+            {times}
+          </tbody>
         </Grid>
       </div>
       <ButtonBox>
-        <Button>{'<'}</Button>
-        <Button>{'>'}</Button>
+        <Button key={'left'}>{'<'}</Button>
+        <Button key={'right'}>{'>'}</Button>
       </ButtonBox>
+      <br />
+      {edit && <Button onClick={() => processTimes()}>Submit</Button>}
     </Wrapper>
   );
 }
--- frontend/src/pages/Meeting.js
+++ frontend/src/pages/Meeting.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import { getMeetingData } from '../api';
+import { getMeetingData, inputUserTimes } from '../api';
 import styled from 'styled-components';
 import { Calendar, Layout } from '@components';
 import { Title } from '@styles';
@@ -11,21 +11,42 @@ const CalendarWrapper = styled.div`
   justify-content: space-around;
 `;
 
+const NameInput = styled.input`
+  width: 40%;
+  margin-left: 2rem;
+`;
+
 export default function Meeting() {
+
   const [meetingData, setMeetingData] = React.useState({});
+
+  const [ userName, setUserName ] = React.useState('');
 
   let { meetingID, password } = useParams();
   console.log(meetingID + ' ' + password);
 
   React.useEffect(
-    () => getMeetingData(meetingID, password, setMeetingData),
+    () => {
+      getMeetingData(meetingID, password, setMeetingData);
+      console.log(meetingData);
+    },
     []
   );
+
+  React.useEffect(
+    () => {
+      console.log(userName);
+    },
+    [userName]
+  );
+
+  const submitUserTimes = (times) => {
+    inputUserTimes(userName, times);
+  }
 
   // Placeholder values for calendar
   const dates = [];
   const times = [];
-  const edit = true;
 
   return (
     <Layout>
@@ -34,9 +55,13 @@ export default function Meeting() {
 
       <br />
 
+      <NameInput type='text' value={userName} onChange={(event) => setUserName(event.target.value)}/>
+
+      <br />
+
       <CalendarWrapper>
-        <Calendar dates times edit />
-        <Calendar dates times edit />
+        <Calendar key={'input'} meetingData submitUserTimes={(timeValues) => submitUserTimes(timeValues)} edit={true} />
+        <Calendar key={'output'} meetingData edit={false} />
       </CalendarWrapper>
     </Layout>
   );
